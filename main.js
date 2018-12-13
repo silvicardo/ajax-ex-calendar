a313ce399ed0540735f48111758d0cca688dddcb
silvicardo <silvicardo@gmail.com>
2018-12-13 18:17:15 +0100

    JS statically manages days in January 2017 with holidayAPI data

--- main.js
+++ main.js
@@ -15,6 +15,146 @@ Holiday API: https://holidayapi.com/ (required params: key, country, year, month
 // TEST URL : https://holidayapi.com/v1/holidays?key=08c8428e-02cd-43c9-bd37-680414486a2d&country=US&year=2017&month=12
 //required params: key, country, year, month
 
-$(document).ready(function(){
-console.log('welcome to ajax-ex-calendar');
-})
+//PROGRAMMA STATICO CON GENNAIO 2017
+
+$(document).ready(function () {
+  console.log('welcome to ajax-ex-calendar');
+
+  $.ajax({
+    url: 'https://holidayapi.com/v1/holidays?key=08c8428e-02cd-43c9-bd37-680414486a2d&country=IT&year=2017&month=1',
+    method: 'GET',
+    success: function (apiData) {
+
+      var meseScelto = creaMeseDa(2017,1);
+
+      meseScelto.settimane = completaDati(meseScelto, apiData.holidays);
+
+      console.log(meseScelto);
+
+      //produci h1 .selected_month
+      mostraTitoloPer(meseScelto);
+
+      //produci gli li per il mese corrente
+      mostraListaGiorniPer(meseScelto);
+
+    },
+    error: function(error){
+      console.log(error);
+    }
+  })
+
+});
+
+//FUNZIONI
+
+//LOGICA(MOMENT.JS)
+
+function creaMeseDa(anno, nrMese) {
+
+  var mese = moment().year(anno).month(nrMese - 1);
+
+  var meseScelto = {
+          nome: mese.format("MMMM"),
+          numeroMeseNellAnno: mese.format('MM'),
+          anno: anno,
+          numeroDiGiorni: mese.daysInMonth(),
+        };
+
+  return meseScelto;
+}
+
+function creaGiorniDel(mese) {
+  var giorni = [];
+
+  for (var i = 1; i <= mese.numeroDiGiorni; i++) {
+    var day = moment(mese.numeroMeseNellAnno + '-' + i + '-' + mese.anno);
+
+    var giorno = {
+      nrGiorno: i,
+      festivo: false,
+      nrGiornoInSettimana: day.format('e'),
+      nomeGiornoInSettimana: day.format('dddd'),
+    }
+
+    giorni.push(giorno);
+  }
+  return giorni;
+}
+
+//LOGICA DATI API
+
+function completaDati(mese, festivita) {
+
+  var giorni = creaGiorniDel(mese);
+
+  for (var i = 0; i < festivita.length; i++) {
+    var festaCorrente = festivita[i];
+    var indiceGiornoDiFesta = (parseInt(festaCorrente.date.split('-')[2])) - 1;
+    giorni[indiceGiornoDiFesta].festivo = true;
+    giorni[indiceGiornoDiFesta].nomeFestivita = festaCorrente.name;
+  }
+
+  var settimane = creaSettimaneDa(giorni);
+
+  return settimane;
+}
+
+function creaSettimaneDa(giorni) {
+  var settimane = [[]];
+  var numeroSettimana = 0;
+
+  for (var i = 0; i < giorni.length; i++) {
+    if (i != 0 && giorni[i].nrGiornoInSettimana == 0) {
+      numeroSettimana++;
+      settimane[numeroSettimana] = [];
+    }
+
+    settimane[numeroSettimana].push(giorni[i]);
+  }
+
+  return settimane;
+}
+
+//INTERFACCIA
+
+function mostraTitoloPer(mese) {
+  var htmlTemplateMese = $('#selectedMonth').html();
+  var template = Handlebars.compile(htmlTemplateMese);
+  var data = {
+    month: mese.nome,
+    year: mese.anno
+  }
+  var htmlRisultato = template(data);
+
+  $('.month').append(htmlRisultato)
+}
+
+function mostraListaGiorniPer(mese) {
+
+  var giorniDelMese = [];
+  for (var i = 0; i < mese.settimane.length; i++) {
+    giorniDelMese = giorniDelMese.concat(mese.settimane[i]);
+  }
+
+  for (var i = 0; i < giorniDelMese.length; i++) {
+    var htmlTemplateGiorno = $('#day').html();
+    var template = Handlebars.compile(htmlTemplateGiorno);
+    var data = {
+      number: giorniDelMese[i].nrGiorno,
+      month: mese.nome,
+    };
+    if (giorniDelMese[i].festivo) {
+      data.festivity = giorniDelMese[i].nomeFestivita;
+    }
+
+    var htmlRisultato = template(data);
+
+    $('.days').append(htmlRisultato);
+
+    if (giorniDelMese[i].festivo) {
+      $('.days').children('.day_item').last().addClass('holiday');
+    }
+
+  }
+
+}
